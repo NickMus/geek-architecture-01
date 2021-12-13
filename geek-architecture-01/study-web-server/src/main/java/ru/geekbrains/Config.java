b91746df5624bf1c0129daaea4dc218461e4e0ea
nmusi <n.musinov@gmail.com>
2021-12-13 13:00:10 +0300

    Initial commit

---
+++ geek-architecture-01/study-web-server/src/main/java/ru/geekbrains/Config.java
@@ -0,0 +1,6 @@
+package ru.geekbrains;
+
+public class Config {
+    public static final String WWW = "C:\\Users\\nmusi\\Downloads\\geek-architecture-01\\geek-architecture-01\\www";
+    public static final int PORT = 8080;
+}
